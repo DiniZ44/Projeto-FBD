2c97a1a30754338fb6894bdd268ee3ec088f912b
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-10-20 14:14:39 -0300

    Projeto FBD Incompleto

---
+++ src/br/com/sysfarm/business/ICategoriasBusiness.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sysfarm.business;
+
+/**
+ *
+ * @author santo
+ */
+public interface ICategoriasBusiness {
+    
+}
